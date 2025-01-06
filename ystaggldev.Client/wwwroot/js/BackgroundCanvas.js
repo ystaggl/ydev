15af74602cfd24ffe832742ea9ca39e44035c4ce
ystaggl <yannstaggl@gmail.com>
2025-01-06 20:03:31 +0100

    added canvas, to be used for background animation, as well as devtest page to work on it in peace.

---
+++ ystaggldev.Client/wwwroot/js/BackgroundCanvas.js
@@ -0,0 +1,13 @@
+﻿function BackgroundCanvas(id) {
+    let canvas = document.getElementById(id);
+
+    if (canvas == null) {
+        return false;
+    }
+
+    let ctx = canvas.getContext("2d");
+    ctx.fillStyle = "red";
+    ctx.fillRect(10, 10, 20, 20);
+    ctx.fillStyle = "blue";
+    ctx.fillRect(20, 20, 20, 20);
+}
